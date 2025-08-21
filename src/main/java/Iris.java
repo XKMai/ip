09c297ec39cea4dd3765de15247c768f90e307b1
XKMai <xkmai014@gmail.com>
2025-08-21 16:33:20 +0800

    Added input function that takes in a message and decides what to do with it

--- src/main/java/Iris.java
+++ src/main/java/Iris.java
@@ -10,8 +10,23 @@ public class Iris {
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
 
+
+    private static void input(String message) {
+        if (message == null || message.isEmpty()) {
+            System.out.println("Input cannot be empty.");
+            return;
+        }
+        else if (message == "bye") {
         //Exit Message
         System.out.println("Bye. Hope to see you again soon!");
+            return;
+        }
+        else{
+            echo(message);
+            String response = scanner.nextLine();
+            return input(response);
+        }
+    }
 
     private static void echo(String message) {
         System.out.println(message);
