81908d8b2e112f9591b01ff48ca1e0ffd2159f72
XKMai <xkmai014@gmail.com>
2025-08-21 15:13:18 +0800

    Added exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,12 @@ public class Iris {
                 + "|   \_  __ \  |/  ___/\n"
                 + "|   ||  | \/  |\___ \ \n"
                 + "|___||__|  |__/____  >\n"
-                + "                   \/ \n"
-        System.out.println("Hello from\n" + logo);
+                + "                   \/ \n";
+        //Welcome Message
+        System.out.println("Hello! I'm\n" + logo);
+        System.out.println("What can I do for you?");
+
+        //Exit Message
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
