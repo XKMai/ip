a0d66567dde5a76162ac8c1e2d096ad6aa3e0d68
XKMai <xkmai014@gmail.com>
2025-08-30 14:44:50 +0800

    Task.java: Add header comments

--- src/main/java/iris/Task.java
+++ src/main/java/iris/Task.java
@@ -1,5 +1,6 @@
 package iris;
 
+// Abstract class representing a general task
 public abstract class Task {
     protected String description;
     protected boolean isDone;
@@ -9,13 +10,16 @@ public abstract class Task {
         this.isDone = false;
     }
 
+    // Marks the task as done or not done
     public void markDone() { isDone = true; }
     public void markUndone() { isDone = false; }
 
+    // Returns status icon for display
     protected String getStatusIcon() {
         return isDone ? "[X]" : "[ ]";
     }
 
     public abstract String toString();
+    // Format for saving to file
     public abstract String toSaveFormat();
 }
