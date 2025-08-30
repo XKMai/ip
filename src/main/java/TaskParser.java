8e76368eb05df07c1ae7afc33bb55dad6a4dee58
XKMai <xkmai014@gmail.com>
2025-08-30 12:51:01 +0800

    TaskParser.java: Add TaskParser handler for loading data

---
+++ src/main/java/TaskParser.java
@@ -0,0 +1,27 @@
+public class TaskParser {
+    public static Task parseTask(String line) {
+        String[] parts = line.split(" \\| ");
+        String type = parts[0];
+        boolean isDone = parts[1].equals("1");
+
+        switch (type) {
+            case "T":
+                Todo todo = new Todo(parts[2]);
+                if (isDone) todo.markDone();
+                return todo;
+
+            case "D":
+                Deadline dl = new Deadline(parts[2], parts[3]);
+                if (isDone) dl.markDone();
+                return dl;
+
+            case "E":
+                Event ev = new Event(parts[2], parts[3], parts[4]);
+                if (isDone) ev.markDone();
+                return ev;
+
+            default:
+                throw new IllegalArgumentException("Unknown task type in file: " + type);
+        }
+    }
+}
