e4a5f5cc13800cdcf461b2bfe3aa98a0a2d9403c
XKMai <xkmai014@gmail.com>
2025-08-30 14:45:08 +0800

    Ui.java: Add header comments

--- src/main/java/iris/Ui.java
+++ src/main/java/iris/Ui.java
@@ -2,13 +2,16 @@ package iris;
 
 import java.util.Scanner;
 
+// Handles user interactions
 public class Ui {
     private Scanner scanner;
 
+    // Default constructor initializes the scanner
     public Ui() {
         scanner = new Scanner(System.in);
     }
 
+    // Displays the welcome message with the provided logo
     public void showWelcome(String logo) {
         printLine();
         System.out.println("Hello! I'm\n" + logo);
@@ -16,26 +19,32 @@ public class Ui {
         printLine();
     }
 
+    // Displays the exit message
     public void showExit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    // Displays a line separator
     public void showLine() {
         printLine();
     }
 
+    // Displays an error message
     public void showError(String message) {
         System.out.println("Error! " + message);
     }
 
+    // Reads a command from the user
     public String readCommand() {
         return scanner.nextLine();
     }
 
+    // Displays a general message
     public void showMessage(String msg) {
         System.out.println(msg);
     }
 
+    // Prints a line separator
     private void printLine() {
         System.out.println("____________________________________________________________");
     }
