8aa4f96c4bfc921450b793fec2ec5a877957d8d6
XKMai <xkmai014@gmail.com>
2025-09-13 17:15:51 +0800

    Contact.java: Create Contact class to store information

---
+++ src/main/java/iris/Contact.java
@@ -0,0 +1,30 @@
+package iris;
+
+public class Contact {
+    protected String name;
+    protected String phoneNumber;
+    protected String email;
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Phone: " + phoneNumber + ", Email: " + email;
+    }
+}
